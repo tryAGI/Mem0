2bd07fa861d7cecd988a6083ef18bd72339ba048
Konstantin S. <havendv@gmail.com>
2026-06-26 20:53:43 +0400

    feat: Updated OpenAPI spec (#45)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Mem0/Generated/Mem0.Models.MemoriesBatchDeleteRequestMemorie.g.cs
@@ -0,0 +1,47 @@
+
+#nullable enable
+
+namespace Mem0
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class MemoriesBatchDeleteRequestMemorie
+    {
+        /// <summary>
+        /// The unique identifier of the memory to delete.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("memory_id")]
+        [global::System.Text.Json.Serialization.JsonRequired]
+        public required global::System.Guid MemoryId { get; set; }
+
+        /// <summary>
+        /// Additional properties that are not explicitly defined in the schema
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonExtensionData]
+        public global::System.Collections.Generic.IDictionary<string, object> AdditionalProperties { get; set; } = new global::System.Collections.Generic.Dictionary<string, object>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemoriesBatchDeleteRequestMemorie" /> class.
+        /// </summary>
+        /// <param name="memoryId">
+        /// The unique identifier of the memory to delete.
+        /// </param>
+#if NET7_0_OR_GREATER
+        [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
+#endif
+        public MemoriesBatchDeleteRequestMemorie(
+            global::System.Guid memoryId)
+        {
+            this.MemoryId = memoryId;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemoriesBatchDeleteRequestMemorie" /> class.
+        /// </summary>
+        public MemoriesBatchDeleteRequestMemorie()
+        {
+        }
+
+    }
+}
---
+++ src/libs/Mem0/Generated/Mem0.Models.MemoriesBatchUpdateRequestMemorieMetadata.g.cs
@@ -0,0 +1,19 @@
+
+#nullable enable
+
+namespace Mem0
+{
+    /// <summary>
+    /// Updated metadata to associate with the memory.
+    /// </summary>
+    public sealed partial class MemoriesBatchUpdateRequestMemorieMetadata
+    {
+
+        /// <summary>
+        /// Additional properties that are not explicitly defined in the schema
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonExtensionData]
+        public global::System.Collections.Generic.IDictionary<string, object> AdditionalProperties { get; set; } = new global::System.Collections.Generic.Dictionary<string, object>();
+
+    }
+}
